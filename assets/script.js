515f99faeb4be6c511cb74d48c4e4d87d1bc0ad5
Ascend Ho <coderhs@foxmail.com>
2023-02-12 21:13:59 +0800

    comment script and change order of 'You can find me'

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const APIURL = 'https://api.github.com/users/ascendho';
-const bio = document.querySelector(".bio");
+// const bio = document.querySelector(".bio");
 
 
-fetch(APIURL).then(function (response) {
-    // console.log(response);
-    return response.json();
-}).then(function (data) {
-    // console.log(data)
-    // console.log(data.bio)
-    bio.textContent+=data.bio;
-})
+// fetch(APIURL).then(function (response) {
+//     // console.log(response);
+//     return response.json();
+// }).then(function (data) {
+//     // console.log(data)
+//     // console.log(data.bio)
+//     bio.textContent+=data.bio;
+// })
 
